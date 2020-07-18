9eb7e6d86dffa32ad102b4d194ddc95a7970d6ca
Joao Gilberto Magalhães <joao@byjg.com.br>
2020-07-17 19:17:29 -0500

    Added log

--- src/go-server.go
+++ src/go-server.go
@@ -1,13 +1,37 @@
 package main
 
 import (
-  "net/http"
+	"log"
+	"net/http"
 )
 
-func main() {
-  http.Handle("/", http.FileServer(http.Dir("/static")))
-  if err := http.ListenAndServe(":8080", nil); err != nil {
-    panic(err)
-  }
+type StatusRespWr struct {
+	http.ResponseWriter // We embed http.ResponseWriter
+	status              int
 }
 
+func (w *StatusRespWr) WriteHeader(status int) {
+	w.status = status // Store the status for our own use
+	w.ResponseWriter.WriteHeader(status)
+}
+
+func wrapHandler(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		srw := &StatusRespWr{ResponseWriter: w}
+
+		h.ServeHTTP(srw, r)
+		log.Printf("%s - \"%s %s %s\" %d 0 \"%s\"", r.RemoteAddr, r.Method, r.RequestURI, r.Proto, srw.status, r.UserAgent())
+	}
+}
+
+func main() {
+	http.HandleFunc("/", wrapHandler(
+		http.FileServer(http.Dir("/static"))))
+
+	log.Printf("byjg/static-httpserver")
+	log.Printf("Listen on 8080")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
