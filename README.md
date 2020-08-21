f029894e1335495e605ecb7e1cbc426167a0e23a
Joao Gilberto Magalhaes <joao@byjg.com.br>
2020-08-21 00:43:21 -0500

    Adding coming soon template

--- README.md
+++ README.md
@@ -8,18 +8,41 @@ A really minimal HTTP Server for static files written in GO
 * HTTP to serve few files
 * Really small - less than 15MB !!
 
+## Tags
+
+- latest - The latest image with a coming soon template
+- tiny - A really minimalist image. You need to replace the volume ot build your own on top of this one.  
+
 ## How to use?
 
+The image has the coming soon template and can be customized by setting the environment variables:
+- HTML_TITLE
+- TITLE
+- MESSAGE
+- BG_IMAGE
+- FACEBOOK
+- TWITTER
+- YOUTUBE
+
+e.g.
+
+```bash
+docker run -p 8080:8080 -e TITLE=soon -e "MESSAGE=Keep In Touch" byjg/static-httpserver
 ```
-docker run -p 8080:8080 -v /path/to/local/html:/static byjg/static-httpserver
+
+## Changing the volume
+
 ```
+docker run -p 8080:8080 -v /path/to/local/html:/static byjg/static-httpserver:tiny
+```
+
 
 ## Create your own image
 
 Dockerfile
 
 ```
-FROM byjg/static-httpserver:latest
+FROM byjg/static-httpserver:tiny
 
 COPY /path/to/html /static
 ```
--- src/server.go
+++ src/server.go
@@ -1,39 +1,102 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
 )
 
 type StatusRespWr struct {
-    http.ResponseWriter // We embed http.ResponseWriter
-    status              int
+	http.ResponseWriter // We embed http.ResponseWriter
+	status              int
 }
 
 func (w *StatusRespWr) WriteHeader(status int) {
-    w.status = status // Store the status for our own use
-    w.ResponseWriter.WriteHeader(status)
+	w.status = status // Store the status for our own use
+	w.ResponseWriter.WriteHeader(status)
 }
 
 func wrapHandler(h http.Handler) http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        srw := &StatusRespWr{ResponseWriter: w}
+	return func(w http.ResponseWriter, r *http.Request) {
+		srw := &StatusRespWr{ResponseWriter: w}
 
-        h.ServeHTTP(srw, r)
+		h.ServeHTTP(srw, r)
 
-        log.Printf("%s - \"%s %s %s\" %d %d \"%s\"", r.RemoteAddr, r.Method, r.RequestURI,
-            r.Proto, srw.status, r.ContentLength, r.UserAgent())
-    }
+		log.Printf("%s - \"%s %s %s\" %d %d \"%s\"", r.RemoteAddr, r.Method, r.RequestURI,
+			r.Proto, srw.status, r.ContentLength, r.UserAgent())
+	}
+}
+
+type Variables struct {
+	HtmlTitle string
+	Title     string
+	Message   string
+	Image     string
+	Facebook  string
+	Twitter   string
+	Youtube   string
+}
+
+func getEnvOrDefault(env string, def string) string {
+	if os.Getenv(env) == "" {
+		return def
+	}
+	return os.Getenv(env)
+}
+
+func parseIndex() {
+	index := "/static/index.html"
+
+	if _, err := os.Stat(index); err == nil {
+
+		// Get the environment variables
+		environment := Variables{
+			getEnvOrDefault("HTML_TITLE", "Coming soon"),
+			getEnvOrDefault("TITLE", "soon"),
+			getEnvOrDefault("MESSAGE", "Our website is <span class=\"m1-txt2\">Coming Soon</span>, follow us for update now!"),
+			os.Getenv("BG_IMAGE"),
+			os.Getenv("FACEBOOK"),
+			os.Getenv("TWITTER"),
+			os.Getenv("YOUTUBE")}
+
+		// Read entire index content
+		content, err := ioutil.ReadFile(index)
+		if err != nil {
+			panic(err)
+		}
+		text := string(content)
+
+		// Save back
+		f, err := os.Create(index)
+		if err != nil {
+			panic(err)
+		}
+
+		// Template
+		tmpl, err := template.New("index").Parse(text)
+		if err != nil {
+			panic(err)
+		}
+		err = tmpl.Execute(f, environment)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 func main() {
-    http.HandleFunc("/", wrapHandler(
-        http.FileServer(http.Dir("/static"))))
+	http.HandleFunc("/", wrapHandler(
+		http.FileServer(http.Dir("/static"))))
 
-    log.Printf("byjg/static-httpserver")
-    log.Printf("Listen on 8080")
+	parseIndex()
 
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        panic(err)
-    }
+	// Start server
+	log.Printf("byjg/static-httpserver")
+	log.Printf("Listen on 8080")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
