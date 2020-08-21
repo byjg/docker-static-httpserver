19928abf70e3c1457d084e0d0c57c31e7970316d
Joao Gilberto Magalhaes <joao@byjg.com.br>
2020-08-20 23:38:03 -0500

    Removing tabs

--- src/server.go
+++ src/server.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-	"log"
-	"net/http"
+    "log"
+    "net/http"
 )
 
 type StatusRespWr struct {
-	http.ResponseWriter // We embed http.ResponseWriter
-	status              int
+    http.ResponseWriter // We embed http.ResponseWriter
+    status              int
 }
 
 func (w *StatusRespWr) WriteHeader(status int) {
-	w.status = status // Store the status for our own use
-	w.ResponseWriter.WriteHeader(status)
+    w.status = status // Store the status for our own use
+    w.ResponseWriter.WriteHeader(status)
 }
 
 func wrapHandler(h http.Handler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		srw := &StatusRespWr{ResponseWriter: w}
+    return func(w http.ResponseWriter, r *http.Request) {
+        srw := &StatusRespWr{ResponseWriter: w}
 
-		h.ServeHTTP(srw, r)
+        h.ServeHTTP(srw, r)
 
-		log.Printf("%s - \"%s %s %s\" %d %d \"%s\"", r.RemoteAddr, r.Method, r.RequestURI,
-			r.Proto, srw.status, r.ContentLength, r.UserAgent())
-	}
+        log.Printf("%s - \"%s %s %s\" %d %d \"%s\"", r.RemoteAddr, r.Method, r.RequestURI,
+            r.Proto, srw.status, r.ContentLength, r.UserAgent())
+    }
 }
 
 func main() {
-	http.HandleFunc("/", wrapHandler(
-		http.FileServer(http.Dir("/static"))))
+    http.HandleFunc("/", wrapHandler(
+        http.FileServer(http.Dir("/static"))))
 
-	log.Printf("byjg/static-httpserver")
-	log.Printf("Listen on 8080")
+    log.Printf("byjg/static-httpserver")
+    log.Printf("Listen on 8080")
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
-	}
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        panic(err)
+    }
 }
